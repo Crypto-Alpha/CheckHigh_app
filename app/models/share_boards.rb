e7fb0437d05a63bf835eef7ff11f1fd0b85d31d9
chinyakao <riley.kao@iss.nthu.edu.tw>
2021-06-04 14:09:13 +0800

    fix rubocop

--- app/models/share_boards.rb
+++ app/models/share_boards.rb
@@ -8,7 +8,8 @@ module CheckHigh
     attr_reader :all
 
     def initialize(shareboards_list)
-      return @all = [] if shareboards_list.length.zero?
+      @all = [] unless shareboards_list.length
+
       @all = shareboards_list.map do |share_board|
         ShareBoard.new(share_board)
       end
