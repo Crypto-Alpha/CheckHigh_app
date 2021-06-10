4f1b466b5e870db7457f1cfe341f8f6a50a39a46
chinyakao <riley.kao@iss.nthu.edu.tw>
2021-06-10 10:12:53 +0800

    share_board checking page

--- app/services/get_shareboard_detail.rb
+++ app/services/get_shareboard_detail.rb
@@ -3,14 +3,14 @@
 require 'http'
 
 # Returns all share boards belonging to an account
-class GetAllShareBoardDetail
+class GetShareBoardDetail
   def initialize(config)
     @config = config
   end
 
   def call(current_account, id)
     response = HTTP.auth("Bearer #{current_account.auth_token}")
-                   .get("#{@config.API_URL}/share_boards/#{id}/assignments")
+                   .get("#{@config.API_URL}/share_boards/#{id}")
 
     response.code == 200 ? JSON.parse(response.to_s)['data'] : nil
   end
