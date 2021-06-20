3f3f461af22304ea6fab3cf072b0ac437497bb62
chinyakao <riley.kao@iss.nthu.edu.tw>
2021-06-21 02:56:58 +0800

    update sso

--- app/controllers/auth.rb
+++ app/controllers/auth.rb
@@ -18,19 +18,19 @@ module CheckHigh
     def google_oauth_url(config, call_back_uri)
       url = config.GOOGLE_OAUTH_URL
       client_id = config.GOOGLE_CLIENT_ID
-      scope = config.GOOGLE_SCOPE 
+      scope = config.GOOGLE_SCOPE
 
       "#{url}?client_id=#{client_id}&scope=#{scope}&response_type=code&access_type=offline&include_granted_scopes=true&redirect_uri=#{call_back_uri}"
     end
 
     route('auth') do |routing|
-      @oauth_callback = '/auth/sso_callback'
+      @oauth_callback = '/auth/github_sso_callback'
       @google_callback_uri = "#{App.config.APP_URL}/auth/google_sso_callback"
       @login_route = '/auth/login'
       routing.is 'login' do
         # GET /auth/login
         routing.get do
-          view :login, locals: { gh_oauth_url: gh_oauth_url(App.config) , google_oauth_url: google_oauth_url(App.config, @google_callback_uri) }
+          view :login, locals: { gh_oauth_url: gh_oauth_url(App.config), google_oauth_url: google_oauth_url(App.config, @google_callback_uri) }
         end
 
         # POST /auth/login
@@ -65,8 +65,8 @@ module CheckHigh
         end
       end
 
-      routing.is 'sso_callback' do
-        # GET /auth/sso_callback
+      routing.is 'github_sso_callback' do
+        # GET /auth/github_sso_callback
         routing.get do
           authorized = AuthorizeGithubAccount
                        .new(App.config)
@@ -86,7 +86,7 @@ module CheckHigh
           response.status = 403
           routing.redirect @login_route
         rescue StandardError => e
-          puts "SSO LOGIN ERROR: #{e.inspect}\n#{e.backtrace}"
+          puts "GITHUB SSO LOGIN ERROR: #{e.inspect}\n#{e.backtrace}"
           flash[:error] = 'Unexpected API Error'
           response.status = 500
           routing.redirect @login_route
@@ -121,7 +121,6 @@ module CheckHigh
           routing.redirect @login_route
         end
       end
-
 
       # GET /auth/logout
       @logout_route = '/auth/logout'
@@ -175,57 +174,57 @@ module CheckHigh
           response.status = 403
           routing.redirect @register_route
         end
+      end
 
-        @resetpwd_route = '/auth/resetpwd'
-        routing.on 'resetpwd' do
-          routing.is do
-            # GET /auth/resetpwd
-            routing.get do
-              view :resetpwd
-            end
-  
-            # POST /auth/resetpwd
-            routing.post do
-              resetpwd = Form::ResetPwd.new.call(routing.params)
-  
-              if resetpwd.failure?
-                flash[:error] = Form.validation_errors(resetpwd)
-                routing.redirect @resetpwd_route
-              end
-  
-              VerifyResetPwd.new(App.config).call(resetpwd.to_h)
-  
-              flash[:notice] = 'Please check your email for a verification link'
-              routing.redirect '/'
-            rescue StandardError => e
-              puts "ERROR VERIFYING RESET PWD: #{routing.params}\n#{e.inspect}"
-              flash[:error] = 'Please use a valid email address'
+      @resetpwd_route = '/auth/resetpwd'
+      routing.on 'resetpwd' do
+        routing.is do
+          # GET /auth/resetpwd
+          routing.get do
+            view :resetpwd
+          end
+
+          # POST /auth/resetpwd
+          routing.post do
+            resetpwd = Form::ResetPwd.new.call(routing.params)
+
+            if resetpwd.failure?
+              flash[:error] = Form.validation_errors(resetpwd)
               routing.redirect @resetpwd_route
             end
-          end
-  
-          # GET /auth/resetpwd/<token>
-          routing.get(String) do |resetpwd_token|
-            # verify reset token expire or not and get the account email
-            email = VerifyToken.payload(resetpwd_token)
-  
-            # TODO: get username or not
-            # get the account username
-            username = GetUsername.new.call(email)
-            account = email.merge(username)
-  
-            # route to post the pwd
-            action_route = "/account/resetpwd/#{resetpwd_token}"
-  
-            flash.now[:notice] = 'Email Verified! Please choose a new password'
-            view :account_confirm, locals: { account: account,
-                                             action_route: action_route }
-          rescue VerifyToken::ExpiredTokenError
-            flash[:error] = 'The reset password token has expired, please try again.'
-            response.status = 403
+
+            VerifyResetPwd.new(App.config).call(resetpwd.to_h)
+
+            flash[:notice] = 'Please check your email for a verification link'
+            routing.redirect '/'
+          rescue StandardError => e
+            puts "ERROR VERIFYING RESET PWD: #{routing.params}\n#{e.inspect}"
+            flash[:error] = 'Please use a valid email address'
             routing.redirect @resetpwd_route
           end
-        end  
+        end
+
+        # GET /auth/resetpwd/<token>
+        routing.get(String) do |resetpwd_token|
+          # verify reset token expire or not and get the account email
+          email = VerifyToken.payload(resetpwd_token)
+
+          # TODO: get username or not
+          # get the account username
+          username = GetUsername.new.call(email)
+          account = email.merge(username)
+
+          # route to post the pwd
+          action_route = "/account/resetpwd/#{resetpwd_token}"
+
+          flash.now[:notice] = 'Email Verified! Please choose a new password'
+          view :account_confirm, locals: { account: account,
+                                            action_route: action_route }
+        rescue VerifyToken::ExpiredTokenError
+          flash[:error] = 'The reset password token has expired, please try again.'
+          response.status = 403
+          routing.redirect @resetpwd_route
+        end
       end
     end
   end
